15279d0a813ef6ba8c2fae7dfaafae8bc45674c2
IdkWhereImGoing <k0rdit@proton.me>
2025-08-11 00:03:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return "Hello, Render!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+@app.route("/get_value", methods=["GET"])
+def get_value():
+    return jsonify({"value": "some_variable"})
